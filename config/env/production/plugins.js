7ef2382969596f5d06f4deb135161c3e1903ae1d
Matheus Damião <matheus.damiaoliveira@gmail.com>
2024-01-08 12:15:14 -0300

    feat: add resend plugin

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -9,4 +9,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "ti@mcdcontabil.com.br",
+        defaultReplyTo: "ti@mcdcontabil.com.br",
+      },
+    },
+  },
 });
